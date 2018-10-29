bf0a92e1890e94991f5126369c5a409231187f37
numairh <44484176+numairh@users.noreply.github.com>
2018-10-29 19:38:31 -0400

    Edited header
    removed recipes, header stuff

--- authentication-final/authentication-final/src/app/app-routing.module.ts
+++ authentication-final/authentication-final/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+/* different site components*/
+
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -8,17 +10,24 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuard } from './auth/auth-guard.service';
+import { AuthGuard } from './auth/auth-guard.service';   /* will redirect to sites only if authenticated */
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent, children: [
+  { path: '', redirectTo: '/signup', pathMatch: 'full' }, /* FRONT PAGE ROUTE */
+
+
+
+  /* { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
     { path: ':id', component: RecipeDetailComponent },
     { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] },
   ] },
+
+
   { path: 'shopping-list', component: ShoppingListComponent },
+  */
+
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
 ];
