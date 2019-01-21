9087d2bfbb036d1761d7699d467a2b123bd1e8ca
zhivko95 <43998646+zhivko95@users.noreply.github.com>
2019-01-21 11:40:26 -0500

    Update sign-in.component.spec.ts

--- AngularUI/src/app/user/sign-in/sign-in.component.spec.ts
+++ AngularUI/src/app/user/sign-in/sign-in.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './sign-in.component';
+import { LoginComponent } from 'sign-in.component';
 
 describe('SignInComponent', () => {
   let component: LoginComponent;
